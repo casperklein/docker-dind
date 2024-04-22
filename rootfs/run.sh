1696f4de50cd030507739db99b02484aab0a542f
casperklein <casperklein@users.noreply.github.com>
2024-04-23 01:19:18 +0200

    comment added

--- rootfs/run.sh
+++ rootfs/run.sh
@@ -5,5 +5,6 @@
 
 # exec /usr/local/bin/dockerd-entrypoint.sh
 
+# https://hub.docker.com/_/docker --> TLS: To disable this image behavior, simply override the container command or entrypoint to run 'dockerd' directly
 # no tcp sockets
 exec /usr/local/bin/dockerd
