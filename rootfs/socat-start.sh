9de31836291ccdcb6cc46c4b218c56d36e01af42
casperklein <casperklein@users.noreply.github.com>
2025-05-14 19:47:31 +0200

    add 'reuseaddr'  to socat

--- rootfs/socat-start.sh
+++ rootfs/socat-start.sh
@@ -6,4 +6,8 @@ until [ -S /var/run/docker.sock ]; do
 done
 
 echo "INFO: Docker daemon started."
-socat -d TCP-LISTEN:2375,fork UNIX-CONNECT:/var/run/docker.sock
+
+# fork        Allow more than 1 connection
+# reuseaddr   Allows other sockets to bind to an address even if parts of it (e.g. the local port) are already in use by socat.
+#             Example: https://stackoverflow.com/a/75118838
+socat -d TCP-LISTEN:2375,fork,reuseaddr UNIX-CONNECT:/var/run/docker.sock
